8af9fbae3fdb79dca43c727465e6e582937e456f
isanthree <h1486636414@163.com>
2021-06-23 17:22:49 +0800

    管理员菜单视图

--- src/com/sbs/test/ViewTest.java
+++ src/com/sbs/test/ViewTest.java
@@ -7,14 +7,25 @@ import com.sbs.view.View;
 import org.junit.Test;
 
 public class ViewTest {
+    // 测试登陆页面视图
     @Test
     public static User indexViewTest() throws Exception {
         User user = View.indexView();
         return user;
     }
 
+    // 测试管理员菜单视图
+    @Test
+    public static int manageMenuTest() throws Exception {
+        int item = View.manageMenuView();
+        System.out.println(item);
+
+        return item;
+    }
+
     public static void main(String[] args) throws Exception {
-        User user = indexViewTest();
-        System.out.println(user);
+//        User user = indexViewTest();
+        int item = manageMenuTest();
+//        System.out.println(user);
     }
 }
--- src/com/sbs/view/View.java
+++ src/com/sbs/view/View.java
@@ -10,14 +10,39 @@ public class View {
 
     // 首页视图
     public static User indexView() {
-        System.out.println("*********************************************************************");
+        System.out.println("************************************************************");
         System.out.println("****************\t\t学生信息管理系统\t\t****************");
         System.out.println("****************\t\t请根据提示操作\t\t****************");
-        System.out.println("****************\t\t请输入账号\t\t****************");
+        System.out.println("****************\t\t请输入账号\t\t********************");
         String uname = input.nextLine();
-        System.out.println("****************\t\t请输入密码\t\t****************");
+        System.out.println("****************\t\t请输入密码\t\t********************");
         String upass = input.nextLine();
 
         return new User(uname, upass);
     }
+
+    // 管理员菜单视图
+    public static int manageMenuView() {
+        System.out.println("************************************************************");
+        System.out.println("****************\t\t欢迎管理员回家\t\t****************");
+        System.out.println("****************\t\t请根据提示操作\t\t****************");
+        System.out.println("****************\t\t0. 退出\t\t********************");
+        System.out.println("****************\t\t1. 添加学生信息\t\t********************");
+        System.out.println("****************\t\t2. 删除学生信息\t\t********************");
+        System.out.println("****************\t\t3. 修改学生信息\t\t********************");
+        System.out.println("****************\t\t4. 查询学生信息\t\t********************");
+        // 获取用户输入的信息
+        String type = input.nextLine();
+        // string 转换成 int
+        int item = Integer.parseInt(type);
+        if (item < 0 || item > 4) {
+            System.out.println("输入错误，请重新输入！");
+            return manageMenuView();
+        }
+
+        System.out.println("************************************************************");
+
+        return item;
+    }
+
 }
